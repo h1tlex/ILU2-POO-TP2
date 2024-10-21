92a6147a8f6a051e447917d8e89e06cca62eb54e
h1tlex <89601876+h1tlex@users.noreply.github.com>
2024-10-22 00:02:05 +0200

    tp finished
    one bug; liste vendeur

--- src/controleur/ControlAcheterProduit.java
+++ src/controleur/ControlAcheterProduit.java
@@ -7,7 +7,6 @@ import villagegaulois.Village;
 
 public class ControlAcheterProduit {
 	private Village village;
-	private Etal etal;
 	private ControlTrouverEtalVendeur controlTrouverEtalVendeur;
 	private ControlVerifierIdentite controlVerifierIdentite;
 
@@ -26,43 +25,14 @@ public class ControlAcheterProduit {
 		}
 		return true;
 	}
-
-	public boolean controlAcheterProduit(String produit) {
-		int vendeurChoisi;
-		Gaulois[] vendeurs = village.rechercherVendeursProduit(produit);
-		boolean achatPossible = false;
-		// si le marché est vide
-		if(vendeurs== null) {
-			System.out.println("Le marché est vide, revenez plus tard.\n");
-		}
-		// si le produit n'existe pas
-		else if(vendeurs.length<1) {
-			System.out.println("Ce produit n'existe pas au marché, revenez plus tard.\n");
-			return achatPossible;
-			
-		} else { // liste des vendeurs qui vend le produit
-			achatPossible = true;
-			System.out.println("Vendeurs qui proposent le produit " + produit + ".\n");
-			for(int i = 1; i <= vendeurs.length ; i++) {
-				System.out.println(i +" - " + vendeurs[i-1] + "\n");
-			}
-		}
-		// choisir vendeur
-		if(achatPossible) {
-			
-			do {
-				vendeurChoisi = Clavier.entrerEntier("Choisir un vendeur de la liste :\n");
-			}while(vendeurChoisi > vendeurs.length || vendeurChoisi < 1); // boucle erreur input
-			
-			// trouver etal vendeur
-			etal = village.rechercherEtal(vendeurs[vendeurChoisi-1]);
-			
-			// choisir nombre de produit à acheter
-			int nbProduitAcheter = Clavier.entrerEntier("Choisir combien de " + produit + "à acheter\n");
-			// acheter produit
-			nbProduitAcheter = etal.acheterProduit(nbProduitAcheter);
-		}
-		
-		return achatPossible;
+	
+	public Gaulois[] getVendeurs(String produit) {
+		return village.rechercherVendeursProduit(produit);
 	}
+	
+	public Etal trouverEtalVendeur(Gaulois nomVendeur) {
+		return village.rechercherEtal(nomVendeur);
+	}
+	
+	
 }
--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -3,18 +3,23 @@ package frontiere;
 import java.util.Scanner;
 
 import controleur.ControlAcheterProduit;
+import personnages.Gaulois;
+import villagegaulois.Etal;
 
 public class BoundaryAcheterProduit {
 	private Scanner scan = new Scanner(System.in);
 	private ControlAcheterProduit controlAcheterProduit;
-
+	private Etal etal;
+	
 	public BoundaryAcheterProduit(ControlAcheterProduit controlAcheterProduit) {
 		this.controlAcheterProduit = controlAcheterProduit;
 	}
 
 	public void acheterProduit(String nomAcheteur) {
-		boolean achatPossible;
+		boolean achatPossible = false;
+		int vendeurChoisi;
 		boolean verification = controlAcheterProduit.verifierIdentite(nomAcheteur);
+		
 		if (!verification) {
 			System.out.println("Je suis désolé " + nomAcheteur + ", il faut être un habitant du village pour commercer ici.\n");
 			
@@ -23,11 +28,40 @@ public class BoundaryAcheterProduit {
 			question.append("Quel produit voulez-vous acheter?\n");
 			String produit = Clavier.entrerChaine(question.toString());
 			
-			achatPossible = controlAcheterProduit.controlAcheterProduit(produit);
+			Gaulois[] vendeurs = controlAcheterProduit.getVendeurs(produit);
 			
+			// si le marché est vide
+			if(vendeurs == null) {
+				System.out.println("Le marché est vide, revenez plus tard.\n");
+			}
+			// si le produit n'existe pas
+			else if(vendeurs.length<1) {
+				System.out.println("Ce produit n'existe pas au marché, revenez plus tard.\n");
+			} else { // liste des vendeurs qui vend le produit
+				achatPossible = true;
+				System.out.println("Vendeurs qui proposent le produit " + produit + ".\n");
+				for(int i = 1; i <= vendeurs.length ; i++) {
+					System.out.println(i +" - " + vendeurs[i-1] + "\n");
+				}
+			}	
+			
+			// choisir vendeur
 			if(achatPossible) {
+				
+				do {
+					vendeurChoisi = Clavier.entrerEntier("Choisir un vendeur de la liste :\n");
+				}while(vendeurChoisi > vendeurs.length || vendeurChoisi < 1); // boucle erreur input
+				
+				// trouver etal vendeur
+				etal = controlAcheterProduit.trouverEtalVendeur(vendeurs[vendeurChoisi-1]);
+				// choisir nombre de produit à acheter
+				int nbProduitAcheter = Clavier.entrerEntier("Choisir combien de " + produit + "à acheter\n");
+				// acheter produit
+				etal.acheterProduit(nbProduitAcheter);
+				
 				System.out.println("Produits acheter avec success ! \n");
-			} else {
+
+		} else {
 				System.out.println("Pas de produits acheter. \n");
 			}
 		}
