a7f0d6a37950a0c684c40f429b77cdfcf14ba7e5
h1tlex <89601876+h1tlex@users.noreply.github.com>
2024-10-22 01:09:06 +0200

    FINAL TP

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -16,8 +16,9 @@ public class BoundaryAfficherMarche {
 			System.out.println("Le marché est vide, revenez plus tard.\n");
 		} else {
 			System.out.println(nomAcheteur + ", vous trouvez au marché: \n");
-			for(int i = 0, j = 0; i< infosMarche.length-2; i++) {
+			for(int i = 0; i< infosMarche.length-2; i+=3) {
 				System.out.println("- vendeur :" + infosMarche[i] + " qui vend " + infosMarche[i+1] + " " + infosMarche[i+2] );
+				
 			}
 		}
 	}
--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -25,13 +25,15 @@ public class BoundaryPrendreEtal {
 		if(!nomVendeurConnu) {
 			System.out.println("Je suis désolée " + nomVendeur + "mais il faut être un habitant du village pour commercer ici.\n");
 		} else {
+			
 			System.out.println("Bonjour " + nomVendeur + ", je vais regarder si je peux vous trouver un étal.\n");
+			
 			etalDisponible = controlPrendreEtal.resteEtals();
 			
 			if(!etalDisponible) {
 				System.out.println("Désolé "+ nomVendeur + " je n'ai plus d'étals qui ne soit pas déjà occupé.");
 			} else {
-				installerVendeur(nomVendeur);
+				//installerVendeur(nomVendeur);
 				
 				text.append("C'est parfait, il me reste un étal pour vous!\n");
 				text.append("Il me faudra quelques renseignements :\n");
@@ -41,7 +43,7 @@ public class BoundaryPrendreEtal {
 				
 				numEtal = controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit);
 				if(numEtal != -1) {
-					System.out.println("Le vendeur "+ nomVendeur + " s'est installé à l'étal n° "+ numEtal);
+					System.out.println("Le vendeur "+ nomVendeur + " s'est installé à l'étal n° "+ numEtal+1);
 				}	
 			}
 		}
