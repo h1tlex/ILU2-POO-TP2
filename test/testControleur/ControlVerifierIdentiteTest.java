4100f38ed1154f7b815931f6a40ef99942aa86df
h1tlex <89601876+h1tlex@users.noreply.github.com>
2024-11-08 12:01:53 +0100

    test VerifierIdentite

---
+++ test/testControleur/ControlVerifierIdentiteTest.java
@@ -0,0 +1,38 @@
+package testControleur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import controleur.ControlVerifierIdentite;
+import personnages.Chef;
+import villagegaulois.Village;
+
+class ControlVerifierIdentiteTest {
+	
+	private Village village;
+	private Chef abraracourcix;
+
+	@BeforeEach
+	public void initialiserSituation() {
+		System.out.println("Initialisation...\n");
+		village = new Village("le village des irréductibles", 10, 5);
+		abraracourcix = new Chef("Abraracourcix",10,village);
+		village.setChef(abraracourcix);
+
+	}
+	@Test
+	void testControlVerifierIdentite() {
+		ControlVerifierIdentite controlVerifierIdentite = new ControlVerifierIdentite(village);
+		assertNotNull(controlVerifierIdentite,"Constructeur ne renvoie pas null");
+	}
+	
+	@Test
+	void testVerifierIdentite() {
+		ControlVerifierIdentite controlVerifierIdentite = new ControlVerifierIdentite(village);
+		assertTrue(controlVerifierIdentite.verifierIdentite("Abraracourcix"));
+		assertFalse(controlVerifierIdentite.verifierIdentite("falseName"));
+	}
+
+}
